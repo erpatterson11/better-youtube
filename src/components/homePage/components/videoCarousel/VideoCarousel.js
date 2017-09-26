2d870860eddb4c456b62fd384e37d998086409f4
Eric Patterson <erpat@Window-To-Erics-Soul.(none)>
2017-09-26 11:06:28 -0500

    updated carousel component

--- src/components/homePage/components/videoCarousel/VideoCarousel.js
+++ src/components/homePage/components/videoCarousel/VideoCarousel.js
@@ -4,6 +4,12 @@ import React, { Component } from 'react'
 
 // COMPONENTS
 import VideoLinkCard from '../../../videoLinkCard/VideoLinkCard'
+import CarouselButton from './components/carouselButton/CarouselButton'
+
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+
+import ArrowRight from 'material-ui/svg-icons/hardware/keyboard-arrow-right'
+import ArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left'
 
 // REDUX
 import { connect } from 'react-redux'
@@ -13,58 +19,167 @@ import { connect } from 'react-redux'
 // CSS
 import './videoCarousel.css'
 
+
+const video = `{"kind":"youtube#searchResult","etag":"'VPWTmrH7dFmi4s1RqrK4tLejnRI/cu5FKMp4GfejPiYj3bEf_2X3kYE'","id":{"kind":"youtube#video","videoId":"2J5GzHoKl1Q"},"snippet":{"publishedAt":"2016-07-15T14:18:42.000Z","channelId":"UC9obdDRxQkmn_4YpcBMTYLw","title":"Dogs, man's best and funniest friends - funny dog compilation","description":"It is never boring with our furry besties! They just don't fail to make our day mor entertaining :D Hope you like our compilation, please share it and SUBSCRIBE!","thumbnails":{"default":{"url":"https://i.ytimg.com/vi/2J5GzHoKl1Q/default.jpg","width":120,"height":90},"medium":{"url":"https://i.ytimg.com/vi/2J5GzHoKl1Q/mqdefault.jpg","width":320,"height":180},"high":{"url":"https://i.ytimg.com/vi/2J5GzHoKl1Q/hqdefault.jpg","width":480,"height":360}},"channelTitle":"Tiger FunnyWorks","liveBroadcastContent":"none"}}`
+const parsedVideo = JSON.parse(video)
+let videos = [parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo]
+
+
 class VideoCarousel extends Component {
 
     // LIFECYCLE FUNCS
-    constructor(props) { // define state and biind methods
-        super(props) // defines props in constructor, reduces bugs
-        this.state = {}
+    constructor(props) { 
+        super(props) 
+
+        this.state = {
+            carouselStyle: {},
+            indexLeft: 0,
+            transform: 0,
+            showScrollRight: "initial",
+            showScrollLeft: "none"
+        }
+
         // BIND CUSTOM FUNCS
-        // ( (comp, funcsList) => {
-        //     funcsList.forEach( func => comp.func = func.bind(comp) )
-        // } )(this, [])
+        this.move = this.move.bind(this)
+        // this.moveLeft = this.moveLeft.bind(this)
+        // this.moveRight = this.moveRight.bind(this)
     }
 
-    // componentWillMount() {} // is run before mounting. setState won't trigger re-render. avoid side-effects or subscriptions
-    // componentDidMount() {} // is run after mounting. target DOM nodes here. load async data here. 
-    // componentWillReceiveProps(nextProps) {} // is run before props changed or parent triggers rerender. use to setState. may run when new props aren't received.
-    // shouldComponentUpdate(nextProps, nextState) {} // is run before props or state change triggers rerender. return false to stop component rerender 
-    // componentWillUpdate(nextProps, nextState) {} // is run after new props or state are received and comp will rerender. can't call setState
-    // componentDidUpdate() {} // is run after initial rerender. operate on DOM. make network requests if state or props changed
-    // componentWillUnmount() {} // is run after component unmounts. cancel timers, event listeners, network requests, destroy manually created DOM elements, etc...
+    componentDidMount() {
+        console.log(this.refs.carousel.clientWidth);
+
+    }
 
     // CUSTOM FUNCS
 
+    move(dir) {
+        const videoCardWidth = 214
+
+        let windowCapacity = ~~(this.refs.carousel.clientWidth / videoCardWidth)
+        let remainingRight = videos.length - this.state.indexLeft - windowCapacity
+        if (dir == "right") {
+
+            // Right
+            let stepsLeft = windowCapacity >= remainingRight ? remainingRight : windowCapacity
+            let transform = this.state.transform - stepsLeft
+            let newIndexLeft = this.state.indexLeft + stepsLeft
+            let showScrollRight = newIndexLeft !== videos.length - windowCapacity ? "initial" : "none"
+            let carouselStyle = {transform: `translateX(${transform * videoCardWidth}px)`}
+            this.setState({
+                carouselStyle, 
+                indexLeft: newIndexLeft, 
+                transform,
+                showScrollRight,
+                showScrollLeft: "initial"
+            })
+
+        } else {
+
+            // Left
+            let stepsRight = this.state.indexLeft < windowCapacity ? this.state.indexLeft : windowCapacity
+            let transform = this.state.transform + stepsRight
+            let newIndexLeft = this.state.indexLeft - stepsRight
+            let showScrollLeft = newIndexLeft ? "initial" : "none"
+
+            let carouselStyle = {transform: `translateX(${transform * videoCardWidth}px)`}
+            this.setState({
+                carouselStyle, 
+                indexLeft: newIndexLeft, 
+                transform,
+                showScrollLeft,
+                showScrollRight: "initial"
+            })
+        }
+    } 
+
+    // moveRight() {
+
+    //     const mediaQueries = {
+    //         1305: 1070,
+    //         1090: 856,
+    //         875: 642,
+    //         660: 428
+    //     }
+
+    //     const videoCardWidth = 214
+
+    //     let windowCapacity = ~~(this.refs.carousel.clientWidth / videoCardWidth)
+
+
+    //     const numInWindow = width => { 
+    //         return ~~(mediaQueries[width] / videoCardWidth) }
+
+    //     // let windowCapacity = numInWindow(1305)
+    //     let remainingRight = videos.length - this.state.indexLeft - windowCapacity
+    //     let stepsLeft = windowCapacity >= remainingRight ? remainingRight : windowCapacity
+    //     let transform = this.state.transform - stepsLeft
+    //     let newIndexLeft = this.state.indexLeft + stepsLeft
+    //     let showScrollRight = newIndexLeft !== videos.length - windowCapacity ? "initial" : "none"
+
+    //     let carouselStyle = {transform: `translateX(${transform * videoCardWidth}px)`}
+    //     this.setState({
+    //         carouselStyle, 
+    //         indexLeft: newIndexLeft, 
+    //         transform,
+    //         showScrollRight,
+    //         showScrollLeft: "initial"
+    //     }, () => console.log(newIndexLeft,videos.length, windowCapacity))
+    // }
+
+    // moveLeft() {
+
+    //     const mediaQueries = {
+    //         1305: 1070,
+    //         1090: 856,
+    //         875: 642,
+    //         660: 428
+    //     }
+
+    //     const videoCardWidth = 214
+
+    //     const numInWindow = width => { 
+    //         return ~~(mediaQueries[width] / videoCardWidth) }
+
+    //     let windowCapacity = ~~(this.refs.carousel.clientWidth / videoCardWidth)
+
+
+    //     // let windowCapacity = numInWindow(1305)
+    //     let stepsRight = this.state.indexLeft < windowCapacity ? this.state.indexLeft : windowCapacity
+    //     let transform = this.state.transform + stepsRight
+    //     let newIndexLeft = this.state.indexLeft - stepsRight
+    //     let showScrollLeft = newIndexLeft ? "initial" : "none"
+
+
+    //     let carouselStyle = {transform: `translateX(${transform * videoCardWidth}px)`}
+    //     this.setState({
+    //         carouselStyle, 
+    //         indexLeft: newIndexLeft, 
+    //         transform,
+    //         showScrollLeft,
+    //         showScrollRight: "initial"
+    //     })
+    // }
+
+
     // RENDER
     render() {
-
-        const video = `{"kind":"youtube#searchResult","etag":"'VPWTmrH7dFmi4s1RqrK4tLejnRI/cu5FKMp4GfejPiYj3bEf_2X3kYE'","id":{"kind":"youtube#video","videoId":"2J5GzHoKl1Q"},"snippet":{"publishedAt":"2016-07-15T14:18:42.000Z","channelId":"UC9obdDRxQkmn_4YpcBMTYLw","title":"Dogs, man's best and funniest friends - funny dog compilation","description":"It is never boring with our furry besties! They just don't fail to make our day mor entertaining :D Hope you like our compilation, please share it and SUBSCRIBE!","thumbnails":{"default":{"url":"https://i.ytimg.com/vi/2J5GzHoKl1Q/default.jpg","width":120,"height":90},"medium":{"url":"https://i.ytimg.com/vi/2J5GzHoKl1Q/mqdefault.jpg","width":320,"height":180},"high":{"url":"https://i.ytimg.com/vi/2J5GzHoKl1Q/hqdefault.jpg","width":480,"height":360}},"channelTitle":"Tiger FunnyWorks","liveBroadcastContent":"none"}}`
-
-        const parsedVideo = JSON.parse(video)
-        const videos = [parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo,parsedVideo]
 
         const renderVideos = function() {
             return videos.map( (vid, i) => <VideoLinkCard key={vid.id.videoId + i} video={vid} vertical /> )
         }
 
         return (
-            <div className="video-carousel-container">
-                {renderVideos()}
+            <div style={{position: "relative"}} >
+                <CarouselButton dir="left" move={() => this.move("left")} show={this.state.showScrollLeft} />
+                <CarouselButton dir="right" move={() => this.move("right")} show={this.state.showScrollRight}/>
+                <div className="video-carousel-container" ref="carousel" style={this.state.carouselStyle} >
+                    {renderVideos()}
+                </div>
             </div>
         )
     }
 }
 
-// REDUX
-
-// function mapStateToProps(state) {
-//     return state
-// }
-
 // EXPORT
 
 export default VideoCarousel
-
-// REDUX EXPORT
-
-// export default connect( mapStateToProps, mapActionsToProps )(VideoCarousel)
--- src/components/videoLinkCard/VideoLinkCard.js
+++ src/components/videoLinkCard/VideoLinkCard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import IconMenu from 'material-ui/IconMenu'
+import MenuItem from 'material-ui/MenuItem'
 import IconButton from 'material-ui/IconButton'
 import MoreVert from 'material-ui/svg-icons/navigation/more-vert'
 
@@ -24,29 +26,38 @@ export default class VideoLinkCard extends Component {
     render() {
 
         let {video, setVideo, vertical} = this.props
-        console.log(video);
         let image = video.snippet.thumbnails.medium
 
         const button = (
-            <IconButton style={{padding: 0, height: "24px", width: "24px"}}>
-                <MoreVert color="rgba(17,17,17,0.4)" hoverColor="rgb(17,17,17)" />
-            </IconButton>
+            <IconMenu 
+                iconButtonElement={
+                    <IconButton style={{padding: 0, height: "24px", width: "24px"}}>
+                        <MoreVert color="rgba(17,17,17,0.4)" hoverColor="rgb(17,17,17)" />
+                    </IconButton>
+                }
+                >
+                <MenuItem primaryText="Not interested" />
+                <MenuItem primaryText="Add to Watch later"/>
+                <MenuItem primaryText="Add to playlist" />
+            </IconMenu>
         )
 
-        const renderButton = () => this.state.buttonIsShown && this.props.vertical ? button : null
+        const buttonPlaceholder = <div style={{width: "24px", height: "24px"}}></div>
+
+        const renderButton = () => this.state.buttonIsShown ? button : null
 
 
         return (
-            <div className={`video-link-card${this.state.isVert}`} onClick={ () => setVideo(video) } onMouseEnter={ () => this.toggleButton(true) } onMouseLeave={ () => this.toggleButton(false) } > 
-                <div className={`video-link-pic${this.state.isVert}`} style={{backgroundImage: `url(${image.url})`}}>
+            <div className={`video-link-card${this.state.isVert}`} onMouseEnter={ () => this.toggleButton(true) } onMouseLeave={ () => this.toggleButton(false) } > 
+                <div className={`video-link-pic${this.state.isVert}`} onClick={ () => setVideo(video) } style={{backgroundImage: `url(${image.url})`}}>
                 </div>
                 <div className={`video-link-text${this.state.isVert}`}>
                     <div className={`video-link-title-container${this.state.isVert}`}>
-                        <p className={`video-link-title${this.state.isVert}`}>{video.snippet.title}</p>
+                        <p className={`video-link-title${this.state.isVert}`} onClick={ () => setVideo(video) } >{video.snippet.title}</p>
                         {renderButton()}
                     </div>
                     <p className={`video-link-channel${this.state.isVert}`}>{video.snippet.channelTitle}</p>
-                    { vertical ? <p className={'video-link-channel-vertical'}>57K views * 2 days ago</p> : null }
+                    { vertical ? <p className={'video-link-details-vertical'}>57K views * 2 days ago</p> : null }
                 </div>
             </div> 
         )
