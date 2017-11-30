197242cfeaf2aaf6e5e56f3550d8b3844e03d39c
Eric Patterson <erpat@Window-To-Erics-Soul.(none)>
2017-11-30 11:51:28 -0600

    added axios to navbar.js

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 import * as DockActions from '../../store/reducers/dockReducer'
 import { getVideoSuggestions } from '../../store/reducers/selectedVideoReducer'
